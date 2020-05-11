84b56c2f52cc7999fedc6178a2397cfd12ad6ad9
MKnoles <mknoles78@gmail.com>
2020-05-10 23:23:38 -0400

    added routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        Home
+      </Route>
+      <Route exact path="/signup">
+        Signup
+      </Route>
+      <Route exact path="/register">
+        Register
+      </Route>
+      <Route exact path="/looks">
+        Looks
+      </Route>
+      <Route exact path="/retailer">
+        Retailer
+      </Route>
+      <Route exact path="/foodcourt">
+        Foodcourt
+      </Route>
+    </Switch>
+  </BrowserRouter>
+  </>
+ 
 }
 
 export default App;
