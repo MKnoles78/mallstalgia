6801f6fbbd133601348acd491605ba075f3e521c
John Kang <pollyfactor@hotmail.com>
2020-05-12 13:02:21 -0400

    added form

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -1,16 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
 
-    state = {};
-    
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <>
+        <form>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="form-group form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" for="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </>
+    );
+  }
 }
 
 export default Login;
