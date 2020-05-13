dbcb513eed5bafa65051269829eeefab44cc24eb
John Kang <pollyfactor@hotmail.com>
2020-05-13 13:47:35 -0400

    updated login page

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -11,7 +11,6 @@ class Login extends Component {
   render() {
     return (
       <>
-      <div container>
         <div className="container">
           <div className="row">
             <div className="col-sm-4"></div>
@@ -47,7 +46,6 @@ class Login extends Component {
             </div>
             <div className="col-sm-4"></div>
           </div>
-        </div>
         </div>
       </>
     );
