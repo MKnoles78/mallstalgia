d4a0cd433cf1d920fb735f7b106940b5ff9a0d29
John Kang <pollyfactor@hotmail.com>
2020-05-14 23:43:18 -0400

    cleaned up code, used Route for props

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, {useState}  from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./containers/Home";
 import Foodcourt from "./containers/Foodcourt";
 import Looks from "./containers/Looks";
@@ -10,38 +10,21 @@ import NavBar from "./components/NavBar/NavBar";
 import NotFound from "./containers/NotFound";
 
 function App() {
-
+  
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-  <BrowserRouter>
-    <NavBar isLoggedIn={isLoggedIn} />
-    <Switch>
-      <Route exact path="/">
-        <Home/>
-      </Route>
-      <Route exact path="/login">
-        <Login/>
-      </Route>
-      <Route exact path="/register">
-        <Register/>
-      </Route>
-      <Route exact path="/looks">
-        <Looks/>
-      </Route>
-      <Route exact path="/retailer">
-        <Retailer/>
-      </Route>
-      <Route exact path="/foodcourt">
-        <Foodcourt/>
-      </Route>
-      <Route exact path="*">
-        <NotFound/>
-      </Route>
-    </Switch>
-  </BrowserRouter>
-)
- 
+    <Router>
+      <NavBar isLoggedIn={isLoggedIn} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/looks" component={Looks} />
+      <Route exact path="/retailer" component={Retailer} />
+      <Route exact path="/foodcourt" component={Foodcourt} />
+      <Route exact path="*" component={NotFound} />
+    </Router>
+  );
 }
 
 export default App;
