f26caeb6ab8a45e4414afb955f0c8547e339f0a7
John Kang <pollyfactor@hotmail.com>
2020-05-15 06:57:43 -0400

    formatted

--- client/src/containers/Retailer.jsx
+++ client/src/containers/Retailer.jsx
@@ -3,42 +3,42 @@ import "./Retailer.css";
 // import axios from "axios";
 
 class Retailer extends Component {
-//   state = {};
+  //   state = {};
 
-// componentDidMount() {
-//     axios.get("")
-//     .then(res => {
-//         console.log(res.data);
-//     }).catch(err => {
-//         console.log(err)
-//     })
-// }
+  // componentDidMount() {
+  //     axios.get("")
+  //     .then(res => {
+  //         console.log(res.data);
+  //     }).catch(err => {
+  //         console.log(err)
+  //     })
+  // }
 
-// componentDidMount() {}
+  // componentDidMount() {}
 
   render() {
     return (
-        <>
-                <div className="container">
+      <>
+        <div className="container">
           <div className="row">
             <div className="col-sm-4"></div>
             <div className="col-sm-4">
               <div className="card">
                 <div className="card-body">
-                <h1>This is my Retail page.</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-          temporibus aut numquam aperiam, harum cum at praesentium dolor
-          voluptatibus ratione iste quaerat quisquam hic placeat reprehenderit
-          illo officiis, voluptate eveniet!
-        </p>
+                  <h1>This is my Retail page.</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+                    temporibus aut numquam aperiam, harum cum at praesentium
+                    dolor voluptatibus ratione iste quaerat quisquam hic placeat
+                    reprehenderit illo officiis, voluptate eveniet!
+                  </p>
                 </div>
               </div>
             </div>
             <div className="col-sm-4"></div>
           </div>
         </div>
-        </>
+      </>
     );
   }
 }
