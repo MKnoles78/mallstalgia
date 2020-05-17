3cfafe1472aaa9b6c2ecffa8bf9ad865d6709a21
nasakib <nat.sakib@gmail.com>
2020-05-17 12:49:20 -0400

    LS attempts

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -24,13 +24,14 @@ class Login extends Component {
   handleSubmit = (event, username, password) => {
     event.preventDefault();
     console.log("username " + username + " password " + password);
-    axios.post("/api/auth", {
-      username,
-      password,
-    })
+    axios
+      .post("/api/auth", {
+        username,
+        password,
+      })
       .then(async (response) => {
         console.log(response.data.success);
-        console.log(response.data.data)
+        console.log(response.data.data);
         if (response.data.success) {
           const decoded = jwt.verify(
             response.data.data,
@@ -42,12 +43,11 @@ class Login extends Component {
         }
       })
       .catch((err) => {
-        console.log(err)
+        console.log(err);
         console.log(err.data);
         this.setState({ error: err.name });
       });
   };
-
 
   render() {
     return (
@@ -95,6 +95,191 @@ class Login extends Component {
       </>
     );
   }
+}  render() {
+    return (
+      <>
+        <div className="container">
+          {this.state.error && (
+            <div
+              className="row"
+              id="alert"
+              style={{ backgroundColor: "#FE6D73", paddingTop: "2" }}
+            >
+              <div
+                className="col"
+                style={{ textAlign: "center", float: "none" }}
+              >
+                <p style={{ color: "#ffffff" }}>{this.state.error}</p>
+              </div>
+            </div>
+          )}
+          <div className="row">
+            <div className="col-sm-4"></div>
+            <div className="col-sm-4">
+              <div className="card">
+                <img
+                  src={logo}
+                  className="card-img-top"
+                  alt="MallStalgia logo"
+                />
+                <div className="card-body">
+                  <Form
+                    handleSubmit={this.handleSubmit}
+                    handleInputChange={this.handleInputChange}
+                    username={this.state.username}
+                    password={this.state.password}
+                  />
+                  <Link to="register" style={{ color: "#FFF" }}>
+                    Need to create an account?
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4"></div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}  render() {
+    return (
+      <>
+        <div className="container">
+          {this.state.error && (
+            <div
+              className="row"
+              id="alert"
+              style={{ backgroundColor: "#FE6D73", paddingTop: "2" }}
+            >
+              <div
+                className="col"
+                style={{ textAlign: "center", float: "none" }}
+              >
+                <p style={{ color: "#ffffff" }}>{this.state.error}</p>
+              </div>
+            </div>
+          )}
+          <div className="row">
+            <div className="col-sm-4"></div>
+            <div className="col-sm-4">
+              <div className="card">
+                <img
+                  src={logo}
+                  className="card-img-top"
+                  alt="MallStalgia logo"
+                />
+                <div className="card-body">
+                  <Form
+                    handleSubmit={this.handleSubmit}
+                    handleInputChange={this.handleInputChange}
+                    username={this.state.username}
+                    password={this.state.password}
+                  />
+                  <Link to="register" style={{ color: "#FFF" }}>
+                    Need to create an account?
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4"></div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}  render() {
+    return (
+      <>
+        <div className="container">
+          {this.state.error && (
+            <div
+              className="row"
+              id="alert"
+              style={{ backgroundColor: "#FE6D73", paddingTop: "2" }}
+            >
+              <div
+                className="col"
+                style={{ textAlign: "center", float: "none" }}
+              >
+                <p style={{ color: "#ffffff" }}>{this.state.error}</p>
+              </div>
+            </div>
+          )}
+          <div className="row">
+            <div className="col-sm-4"></div>
+            <div className="col-sm-4">
+              <div className="card">
+                <img
+                  src={logo}
+                  className="card-img-top"
+                  alt="MallStalgia logo"
+                />
+                <div className="card-body">
+                  <Form
+                    handleSubmit={this.handleSubmit}
+                    handleInputChange={this.handleInputChange}
+                    username={this.state.username}
+                    password={this.state.password}
+                  />
+                  <Link to="register" style={{ color: "#FFF" }}>
+                    Need to create an account?
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4"></div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}  render() {
+    return (
+      <>
+        <div className="container">
+          {this.state.error && (
+            <div
+              className="row"
+              id="alert"
+              style={{ backgroundColor: "#FE6D73", paddingTop: "2" }}
+            >
+              <div
+                className="col"
+                style={{ textAlign: "center", float: "none" }}
+              >
+                <p style={{ color: "#ffffff" }}>{this.state.error}</p>
+              </div>
+            </div>
+          )}
+          <div className="row">
+            <div className="col-sm-4"></div>
+            <div className="col-sm-4">
+              <div className="card">
+                <img
+                  src={logo}
+                  className="card-img-top"
+                  alt="MallStalgia logo"
+                />
+                <div className="card-body">
+                  <Form
+                    handleSubmit={this.handleSubmit}
+                    handleInputChange={this.handleInputChange}
+                    username={this.state.username}
+                    password={this.state.password}
+                  />
+                  <Link to="register" style={{ color: "#FFF" }}>
+                    Need to create an account?
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4"></div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
+
 
 export default Login;
