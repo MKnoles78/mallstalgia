26a6b1e6900ed08984b9bc1230ba121f28027967
MKnoles <mknoles78@gmail.com>
2020-05-17 17:04:53 -0400

    worked towards local access

--- client/src/components/Search/SearchResults/SearchResults.jsx
+++ client/src/components/Search/SearchResults/SearchResults.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import "./SearchResults.css";
-import SearchResult from "./SearchResult/SearchResult"
+import { SearchResult } from "./SearchResult/SearchResult"
 
-export function SearchResults () {
+export function SearchResults (props) {
+
+    if(!props.businesses || !props.businesses.length) {
+        return (<div></div>);
+    }
+
+    const searchResults = props.businesses.map(b => <SearchResult key={b.id} businesses={b}/>)
     return (
         <div className="search-results">
-            <SearchResult />
-            <SearchResult />
+            {searchResults}
         </div>
     );
 };
