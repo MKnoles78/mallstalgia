14149b5b5ac6f3f86e7f5ff72e97e07c1dfd6999
MKnoles <mknoles78@gmail.com>
2020-05-17 17:05:47 -0400

    worked on adding API connects to yelp for display

--- client/src/utils/hooks/YelpAPI/config.js
+++ client/src/utils/hooks/YelpAPI/config.js
@@ -2,4 +2,4 @@ export const API_BASE_URL = 'https://cors-anywhere.herokuapp.com/https://api.yel
 
 
 
-export const BEARER_TOKEN = 'd5URc2xItBGtBtHoSl6mwy1YnNra-TMsDe_GoaYP-14Znm45wDyzAHRarzRvmuv9YrB8dx-oWyro-g-TRDTuWuu11MitNZHQRDEN1ZvvgQXBppgqDGOK_5tddR-_XnYx'
+export const BEARER_TOKEN = ''
